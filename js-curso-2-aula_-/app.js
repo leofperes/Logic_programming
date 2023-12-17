9999e88463a4ec46b153250fc79c0eb3eb986fb0
leofperes <leonardofperess@outlook.com>
2023-12-17 00:36:46 -0300

    Init second course

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo de adivinhar o número';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
+
+function verificarChute(){
+  let chute = document.querySelector('')
+}
---
+++ js-curso-2-aula_-/js-curso-2-desafio_1/app.js
@@ -0,0 +1,23 @@
+let title = document.querySelector('h1')
+title.innerHTML = 'Hora do desafio'
+
+function buttonClicked(){
+  console.log('O botão foi clicado')
+}
+
+function buttonAlert(){
+  alert('Eu amo JS')
+}
+
+function buttonPrompt(){
+  let userCity = prompt('Digite alguma cidade do brasil')
+  alert(`Estive em ${userCity} e lembrei de você`)
+}
+
+function buttonSum(){
+  let number1 = prompt('Digite o primeiro número da soma')
+  let number2 = prompt('Digite o segundo número da soma')
+  let sumNumbers = number1 + number2;
+
+  alert(`O resultado da soma dos números é ${sumNumbers}`)
+}
