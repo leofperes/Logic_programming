ea033e5fb37cea87668b39c937f90cacbcbe26e1
leofperes <leonardofperess@outlook.com>
2024-01-05 22:50:35 -0300

    Finish example of project using the videos

--- js-curso-3/alugames/js/app.js
+++ js-curso-3/alugames/js/app.js
@@ -1,18 +1,37 @@
-let handleButton1 = document.getElementById('game-1').addEventListener("click", alterarStatus)
-let handleButton2 = document.getElementById('game-2').addEventListener("click", alterarStatus)
-let handleButton3 = document.getElementById('game-3').addEventListener("click", alterarStatus)
 
-function exibirTextoNaTela(tag, texto){
-  let campo = document.querySelector(tag);
-  campo.textContent = texto;
-}
+// My solution
 
-function alterarStatus(event){
-    if(event.target.classList.contains('dashboard__item__button--return')){
-      event.target.classList.remove('dashboard__item__button--return')
-      exibirTextoNaTela('a', 'Alugar')
-    } else {
-        event.target.classList.add('dashboard__item__button--return')
-        exibirTextoNaTela('a', 'Devolver')
-    }
+// let handleButton1 = document.getElementById('game-1').addEventListener("click", alterarStatus)
+// let handleButton2 = document.getElementById('game-2').addEventListener("click", alterarStatus)
+// let handleButton3 = document.getElementById('game-3').addEventListener("click", alterarStatus)
+
+// function exibirTextoNaTela(tag, texto){
+//   let campo = document.querySelector(tag);
+//   campo.textContent = texto;
+// }
+
+// function alterarStatus(event){
+//     if(event.target.classList.contains('dashboard__item__button--return')){
+//       event.target.classList.remove('dashboard__item__button--return')
+//       exibirTextoNaTela('a', 'Alugar')
+//     } else {
+//         event.target.classList.add('dashboard__item__button--return')
+//         exibirTextoNaTela('a', 'Devolver')
+//     }
+// }
+
+function alterarStatus(id){
+  let gameClicado = document.getElementById(`game-${id}`);
+  let imagem = gameClicado.querySelector('.dashboard__item__img')
+  let botao = gameClicado.querySelector('.dashboard__item__button')
+
+  if(imagem.classList.contains('dashboard__item__img--rented')){
+    imagem.classList.remove('dashboard__item__img--rented')
+    botao.classList.remove('dashboard__item__button--return')
+    botao.textContent = 'Alugar'
+  } else {
+    imagem.classList.add('dashboard__item__img--rented')
+    botao.classList.add('dashboard__item__button--return')
+    botao.textContent = 'Devolver'
+  }
 }
