a4070ae047653e1f158d5f3b9bdc04dfc6fb2c19
leofperes <leonardofperess@outlook.com>
2024-01-08 17:03:09 -0300

    Fixed my own solution alugames

--- js-curso-3/alugames/js/app.js
+++ js-curso-3/alugames/js/app.js
@@ -1,58 +1,58 @@
 
 // My solution
 
-// let handleButton1 = document.getElementById('game-1').addEventListener("click", alterarStatus)
-// let handleButton2 = document.getElementById('game-2').addEventListener("click", alterarStatus)
-// let handleButton3 = document.getElementById('game-3').addEventListener("click", alterarStatus)
+let handleButton1 = document.getElementById('game-1').addEventListener("click", alterarStatus)
+let handleButton2 = document.getElementById('game-2').addEventListener("click", alterarStatus)
+let handleButton3 = document.getElementById('game-3').addEventListener("click", alterarStatus)
 
-// function exibirTextoNaTela(tag, texto){
-//   let campo = document.querySelector(tag);
-//   campo.textContent = texto;
-// }
-
-// function alterarStatus(event){
-//     if(event.target.classList.contains('dashboard__item__button--return')){
-//       event.target.classList.remove('dashboard__item__button--return')
-//       exibirTextoNaTela('a', 'Alugar')
-//     } else {
-//         event.target.classList.add('dashboard__item__button--return')
-//         exibirTextoNaTela('a', 'Devolver')
-//     }
-// }
-
-let usuario = 'lfp'
-let senha = '123'
-var gameClicado
-let contarAlugados = []
-
-
-function confirmarDevolucao(id){
-  alert('Digite seu usuário e senha')
-  let validacaoUsuario = prompt('Usuário:')
-  let validacaoSenha = prompt('Senha:')
-  gameClicado = document.getElementById(`game-${id}`);
-  if(validacaoUsuario == usuario && validacaoSenha == senha){
-    alterarStatus()
-  } else {
-    alert('Usuário ou senha incorreta!')
-  }
-} 
-
-function alterarStatus(){
-  let imagem = gameClicado.querySelector('.dashboard__item__img')
-  let botao = gameClicado.querySelector('.dashboard__item__button')
-
-  if(imagem.classList.contains('dashboard__item__img--rented')){
-    imagem.classList.remove('dashboard__item__img--rented')
-    botao.classList.remove('dashboard__item__button--return')
-    botao.textContent = 'Alugar'
-    contarAlugados.pop(gameClicado);
-    console.log(contarAlugados.length)
-  } else {
-    imagem.classList.add('dashboard__item__img--rented')
-    botao.classList.add('dashboard__item__button--return')
-    botao.textContent = 'Devolver'
-    contarAlugados.push(gameClicado);
-    console.log(contarAlugados.length)
-  }
+function exibirTextoNaTela(botao, texto){
+  botao.innerHTML = texto
 }
+
+function alterarStatus(event){
+  let botao = event.target
+    if(botao.classList.contains('dashboard__item__button--return')){
+      botao.classList.remove('dashboard__item__button--return')
+      exibirTextoNaTela(botao, 'Alugar')
+    } else {
+      botao.classList.add('dashboard__item__button--return')
+        exibirTextoNaTela(botao, 'Devolver')
+    }
+}
+
+// let usuario = 'lfp'
+// let senha = '123'
+// var gameClicado
+// let contarAlugados = []
+
+
+// function confirmarDevolucao(id){
+//   alert('Digite seu usuário e senha')
+//   let validacaoUsuario = prompt('Usuário:')
+//   let validacaoSenha = prompt('Senha:')
+//   gameClicado = document.getElementById(`game-${id}`);
+//   if(validacaoUsuario == usuario && validacaoSenha == senha){
+//     alterarStatus()
+//   } else {
+//     alert('Usuário ou senha incorreta!')
+//   }
+// } 
+
+// function alterarStatus(){
+//   let imagem = gameClicado.querySelector('.dashboard__item__img')
+//   let botao = gameClicado.querySelector('.dashboard__item__button')
+
+//   if(imagem.classList.contains('dashboard__item__img--rented')){
+//     imagem.classList.remove('dashboard__item__img--rented')
+//     botao.classList.remove('dashboard__item__button--return')
+//     botao.textContent = 'Alugar'
+//     contarAlugados.pop(gameClicado);
+//     console.log(contarAlugados.length)
+//   } else {
+//     imagem.classList.add('dashboard__item__img--rented')
+//     botao.classList.add('dashboard__item__button--return')
+//     botao.textContent = 'Devolver'
+//     contarAlugados.push(gameClicado);
+//     console.log(contarAlugados.length)
+//   }
+// }
