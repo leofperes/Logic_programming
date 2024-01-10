2f6f0a73a856c6d20d57bfcc0f14e0b014b7c5cc
leofperes <leonardofperess@outlook.com>
2024-01-09 21:02:54 -0300

    fixed function to clean wishlist and add validation to add product

--- js-curso-3/carrinho-compras/js/app.js
+++ js-curso-3/carrinho-compras/js/app.js
@@ -1,31 +1,38 @@
-var select = document.getElementById('produto');
+var select = document.getElementById('produto')
 var sectionProducts = document.getElementById('lista-produtos');
 var wishlist;
 var value;
-var spans = [];
 var qtyProducts;
 var valorTotalCarrinho = 0;
+var validarQty
 
 select.addEventListener('change', function(){
   value = select.options[select.selectedIndex].value;
 });
 
+
 function adicionar(){
   quantidadeSelecionada();
-  let nomeProduto = value.split('-')[0]
-  let totalUnitario = somarValoresUnitariosProdutos();
-  let produtoTotal = totalUnitario * qtyProducts;
-  sectionProducts.innerHTML = sectionProducts.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${qtyProducts}X</span> ${nomeProduto} <span class="texto-azul">R$${produtoTotal}</span>
-  </section>`
-  somarValoresProdutos(produtoTotal);
-  return newSpan;
+  validarQty = qtyProducts
+  console.log(validarQty)
+  let textoProduto = select.options[select.selectedIndex].text;
+  if(textoProduto !== "Selecione um produto" && validarQty > 0 ){
+    let nomeProduto = value.split('-')[0]
+    let totalUnitario = somarValoresUnitariosProdutos();
+    let produtoTotal = totalUnitario * qtyProducts;
+    sectionProducts.innerHTML = sectionProducts.innerHTML + `<section class="carrinho__produtos__produto"><span class="texto-azul">${qtyProducts}X</span> ${nomeProduto} <span class="texto-azul">R$${produtoTotal}</span>
+    </section>`
+    somarValoresProdutos(produtoTotal);
+  } else {
+    alert('Selecione um produto ou informe a quantidade desejada')
+  }
 }
 
 function limpar(){
-  for (var i = 0; i < spans.length; i++) {
-    sectionProducts.removeChild(spans[i]);
-  }
-  spans = [];
+  var produtos = sectionProducts.querySelectorAll('.carrinho__produtos__produto');
+  produtos.forEach(function(produto) {
+    sectionProducts.removeChild(produto);
+  });
   valorTotalCarrinho = 0;
   mostrarTotalCarrinho();
 }
@@ -36,7 +43,8 @@ function somarValoresProdutos(totalProduto){
 }
 
 function quantidadeSelecionada(){
-  qtyProducts = document.getElementById('quantidade').value;
+  qtyProducts = parseInt(document.getElementById('quantidade').value, 10) || 0;
+  return qtyProducts
 }
 
 function mostrarTotalCarrinho(){
