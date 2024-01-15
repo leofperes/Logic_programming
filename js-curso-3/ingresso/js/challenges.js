01ba16b60a745a46b1722aca43688a8507a53bcb
leofperes <leonardofperess@outlook.com>
2024-01-15 16:24:55 -0300

    resolved calculator challenge

---
+++ js-curso-3/ingresso/js/challenges.js
@@ -0,0 +1,54 @@
+//#1
+// function convertToString(num){
+//   return parseInt(num)
+// }
+// let stgToInt = convertToString('2312')
+// console.log(stgToInt)
+
+//#2
+let operacao = prompt('Escolha qual operação dessa realizar: Adição, subtração, multiplicação, divisão')
+let num1 = parseInt(prompt('Digite o primeiro número'))
+let num2 = parseInt(prompt('Digite o segundo número'))
+let resultado
+function calcular(e){
+  switch (e) {
+    case 'multiplicação':
+      resultado = multiplicar(num1,num2)
+      alert(`O resultado dos números é ${resultado}`)
+      break;
+    case 'divisão':
+      resultado = dividir(num1, num2)
+      alert(`O resultado dos números é ${resultado}`)
+      break
+    case 'subtração':
+      resultado = subtrair(num1,num2)
+      alert(`O resultado dos números é ${resultado}`)
+      break;
+    case 'adição':
+      resultado = somar(num1, num2)
+      alert(`O resultado dos números é ${resultado}`)
+      console.log('oi')
+      break
+    default:
+      break;
+  }
+}
+
+function multiplicar(x, y){
+  return x * y
+}
+
+function dividir(x, y){
+  return x / y
+}
+
+function subtrair(x, y){
+  return x - y
+}
+
+function somar(x, y){
+  return x + y
+}
+
+calcular(operacao)
+
