c17cbf475dc11346b47029436c8bdc75dfb35701
leofperes <leonardofperess@outlook.com>
2024-01-17 09:41:03 -0300

    finished challenge to check even or odd

--- js-curso-3/ingresso/js/challenges.js
+++ js-curso-3/ingresso/js/challenges.js
@@ -6,49 +6,60 @@
 // console.log(stgToInt)
 
 //#2
-let operacao = prompt('Escolha qual operação dessa realizar: Adição, subtração, multiplicação, divisão')
-let num1 = parseInt(prompt('Digite o primeiro número'))
-let num2 = parseInt(prompt('Digite o segundo número'))
-let resultado
-function calcular(e){
-  switch (e) {
-    case 'multiplicação':
-      resultado = multiplicar(num1,num2)
-      alert(`O resultado dos números é ${resultado}`)
-      break;
-    case 'divisão':
-      resultado = dividir(num1, num2)
-      alert(`O resultado dos números é ${resultado}`)
-      break
-    case 'subtração':
-      resultado = subtrair(num1,num2)
-      alert(`O resultado dos números é ${resultado}`)
-      break;
-    case 'adição':
-      resultado = somar(num1, num2)
-      alert(`O resultado dos números é ${resultado}`)
-      console.log('oi')
-      break
-    default:
-      break;
+// let operacao = prompt('Escolha qual operação dessa realizar: Adição, subtração, multiplicação, divisão')
+// let num1 = parseInt(prompt('Digite o primeiro número'))
+// let num2 = parseInt(prompt('Digite o segundo número'))
+// let resultado
+// function calcular(e){
+//   switch (e) {
+//     case 'multiplicação':
+//       resultado = multiplicar(num1,num2)
+//       alert(`O resultado dos números é ${resultado}`)
+//       break;
+//     case 'divisão':
+//       resultado = dividir(num1, num2)
+//       alert(`O resultado dos números é ${resultado}`)
+//       break
+//     case 'subtração':
+//       resultado = subtrair(num1,num2)
+//       alert(`O resultado dos números é ${resultado}`)
+//       break;
+//     case 'adição':
+//       resultado = somar(num1, num2)
+//       alert(`O resultado dos números é ${resultado}`)
+//       console.log('oi')
+//       break
+//     default:
+//       break;
+//   }
+// }
+
+// function multiplicar(x, y){
+//   return x * y
+// }
+
+// function dividir(x, y){
+//   return x / y
+// }
+
+// function subtrair(x, y){
+//   return x - y
+// }
+
+// function somar(x, y){
+//   return x + y
+// }
+
+// calcular(operacao)
+
+//#3
+let numero = parseInt(evenOrOdd(prompt('Digite um número')))
+
+function evenOrOdd(num) {
+  if (num % 2 === 0) {
+    alert('O número digitado é par')
+  } else {
+    alert('O número digitado é impar')
   }
 }
-
-function multiplicar(x, y){
-  return x * y
-}
-
-function dividir(x, y){
-  return x / y
-}
-
-function subtrair(x, y){
-  return x - y
-}
-
-function somar(x, y){
-  return x + y
-}
-
-calcular(operacao)
 
